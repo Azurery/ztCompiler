3dd7d1be4791f18dc369a65ea526e94b6230e0e6
Azurery <zt838713968@gmail.com>
2017-05-23 23:43:33 +0800

    keyword

--- ZTCompiler/Token.h
+++ ZTCompiler/Token.h
@@ -1,8 +1,50 @@
+#include <set>
+#include <string>
+#include <memory>
 namespace ZTCompiler {
+		//定义c语言的key word
+		enum class keyword {
+			BREAK,
+			CASE,
+			CHAR,
+			CONST,
+			CONTINUE,
+			DEFAULT,
+			DO,
+			WHILE,
+			DOUBLE,
+			IF,
+			ELSE,
+			FOR,
+			INT,
+			LOMG,
+			RETURN,
+			SHORT,
+			STRUCT,
+			SWITCH,
+			UNION,
+			TYPEDEF,
+			VOID
+		};
+		using keyset = std::set<std::string>;
+		class keysetcase {
+		private:
+			static std::shared_ptr<keyset> ptr;
+		public:
+			static std::shared_ptr<keyset> get_instance();
+		};
+
+
 	//token对象应该包含
 	class token {
 		enum class type {
-
+			KEYWORD,
+			NUMBER,
+			STRING,
+			IDENTIFIER,
+			SPACE
 		};
+		
+		
 	};
 }
