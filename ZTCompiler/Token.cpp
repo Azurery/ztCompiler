15bdbff1836cf505de9a9fc27b38a6f786e5c9b3
Azurery <zt838713968@gmail.com>
2017-05-24 09:25:55 +0800

    constructors

--- ZTCompiler/Token.cpp
+++ ZTCompiler/Token.cpp
@@ -1,1 +1,21 @@
 #include "Token.h"
+namespace ZTCompiler {
+	std::shared_ptr<keyset> keyset_instance::ptr = nullptr;
+	std::shared_ptr<keyset> keyset_instance::get_instance() {
+		if (!ptr) {
+			ptr->insert("if");
+			ptr->insert("else");
+			ptr->insert("when");
+			ptr->insert("while");
+			ptr->insert("return");
+			ptr->insert("int");
+			ptr->insert("double");
+			ptr->insert("string");
+			ptr->insert("float");
+			ptr->insert("long");
+			ptr->insert("union");
+			ptr->insert("typedef");
+		}
+		return ptr;
+	}
+}
--- ZTCompiler/Token.h
+++ ZTCompiler/Token.h
@@ -1,50 +1,73 @@
+#ifndef _ZT_TOKEN_H_
+#define _ZT_TOKEN_H_
 #include <set>
 #include <string>
 #include <memory>
 namespace ZTCompiler {
-		//定义c语言的key word
-		enum class keyword {
-			BREAK,
-			CASE,
-			CHAR,
-			CONST,
-			CONTINUE,
-			DEFAULT,
-			DO,
-			WHILE,
-			DOUBLE,
-			IF,
-			ELSE,
-			FOR,
-			INT,
-			LOMG,
-			RETURN,
-			SHORT,
-			STRUCT,
-			SWITCH,
-			UNION,
-			TYPEDEF,
-			VOID
-		};
-		using keyset = std::set<std::string>;
-		class keysetcase {
-		private:
-			static std::shared_ptr<keyset> ptr;
-		public:
-			static std::shared_ptr<keyset> get_instance();
-		};
+	//定义c语言的key word
+	enum class keyword {
+		BREAK,
+		CASE,
+		CHAR,
+		CONST,
+		CONTINUE,
+		DEFAULT,
+		DO,
+		WHILE,
+		DOUBLE,
+		IF,
+		ELSE,
+		FOR,
+		INT,
+		LONG,
+		RETURN,
+		SHORT,
+		STRUCT,
+		SWITCH,
+		UNION,
+		TYPEDEF,
+		VOID
+	};
+
+	using keyset = std::set<std::string>;
+
+	class keyset_instance {
+		friend class Token;
+	private:
+		static std::shared_ptr<keyset> ptr;
+	public:
+		static std::shared_ptr<keyset> get_instance();
+	};
 
 
 	//token对象应该包含
 	class token {
-		enum class type {
+		enum class Type {
+			IDENTIFIER,
 			KEYWORD,
 			NUMBER,
 			STRING,
-			IDENTIFIER,
 			SPACE
 		};
-		
-		
+	private:
+		token() {}
+		token(Type type, std::string value)	{
+			std::set<std::string>::const_iterator key_end = keyset_instance::get_instance()->end();
+			if (type == Type::IDENTIFIER) {
+				char first_char = value.at(0);
+				if (first_char >= '0'&&first_char <= '9')
+					type = Type::NUMBER;
+				else
+					if (keyset_instance::get_instance()->find(value) != key_end) {
+						type = Type::KEYWORD;
+					}
+			}
+		}
+
+	private:
+		std::string value;
+		Type type;
 	};
-}
+
+
+#endif
