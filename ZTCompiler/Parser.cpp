dacbfa60049c6abf0f97afc458029cf3b8670bec
Azurery <zt838713968@gmail.com>
2018-01-07 00:17:43 +0800

    parse integer

--- ZTCompiler/Parser.cpp
+++ ZTCompiler/Parser.cpp
@@ -63,8 +63,50 @@ namespace ztCompiler {
 		return constant::create(token_, flag, static_cast<long>(value));
 	}
 
+	/*
+	(6.4.4.1) integer-suffix:
+				unsigned-suffix long-suffixopt
+				unsigned-suffix long-long-suffix
+				long-suffix unsigned-suffixopt
+				long-long-suffix unsigned-suffixopt
+	(6.4.4.1) unsigned-suffix: one of
+				u U
+	(6.4.4.1) long-suffix: one of
+				l L
+	(6.4.4.1) long-long-suffix: one of
+				ll LL
+*/
 	constant* parser::parse_integer(const token* token_) {
+		const auto& c = token_->str_;
+		long long value;
+		size_t end = 0;
+		try {
+			value = stoll(c);
+		}catch (const std::out_of_range& out_err) {
+			std::cerr << "integer越界" << std::endl;
+		}
+		int flag = 0;
+		for (; c[end]; end++) {
+			if (c[end] == 'u' || c[end] == 'U') {
+				if (flag != static_cast<int>(arithmetic_type::Type_arithmetic_specifier::UNSIGNED))
+					std::cerr << "无效的后缀" << std::endl;
+				flag = static_cast<int>(arithmetic_type::Type_arithmetic_specifier::UNSIGNED);
+			}else {
+				if (flag != static_cast<int>(arithmetic_type::Type_arithmetic_specifier::LONG) ||
+					flag != static_cast<int>(arithmetic_type::Type_arithmetic_specifier::LONG_LONG))
+					std::cerr << "无效的后缀" << std::endl;
+				if (c[end++] == 'l' || c[end++] == 'L') {
+					flag = static_cast<int>(arithmetic_type::Type_arithmetic_specifier::LONG_LONG);
+					++end;
+				}else {
+					flag = static_cast<int>(arithmetic_type::Type_arithmetic_specifier::LONG);
+				}	
+			}
+		}
 
+		if (c[0] > '0'&&c[0] < '9') {
+
+		}
 	}
 
 }
